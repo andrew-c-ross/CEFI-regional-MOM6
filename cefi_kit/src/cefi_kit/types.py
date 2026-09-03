134541847e54cf58a46c92bb734c5188c7f5492b
Andrew C. Ross <andrew.c.ross@noaa.gov>
2026-09-03 09:44:51 -0400

    Keep matplotlib optional

--- cefi_kit/src/cefi_kit/types.py
+++ cefi_kit/src/cefi_kit/types.py
@@ -1,8 +1,6 @@
 from os import PathLike
-from typing import Protocol
+from typing import TYPE_CHECKING, Protocol
 
-from matplotlib.cm import ScalarMappable
-from matplotlib.colorbar import Colorbar
 from numpy.typing import ArrayLike, NDArray
 from xarray import DataArray
 
@@ -10,8 +8,13 @@ __all__ = ['ArrayLike', 'ColorbarCapable', 'NDArray', 'NDArrayLike', 'PathLike']
 
 NDArrayLike = NDArray | DataArray
 
+# Keep matplotlib optional
+if TYPE_CHECKING:
+    from matplotlib.cm import ScalarMappable
+    from matplotlib.colorbar import Colorbar
+
 
 class ColorbarCapable(Protocol):
     """A type (such as matplotlib figure or axes) with a colorbar method"""
 
-    def colorbar(self, mappable: ScalarMappable, **kwargs) -> Colorbar: ...
+    def colorbar(self, mappable: 'ScalarMappable', **kwargs) -> 'Colorbar': ...
